921d42c104a6ebb28b5005559b289032e082f112
nahid361 <khannahi361@gmail.com>
2021-12-01 19:23:44 +0600

    cart button working

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,8 @@ Route::get('/', [PagesController::class, 'index'])->name('home');
 
 Route::get('/shop', [ProductController::class, 'index'])->name('shop');
 
-Route::get('/product/{id}', [ProductController::class, 'show'])->name('product');
+Route::get('/shop/{id}', [ProductController::class, 'show'])->name('product');
 
 Route::get('/cart', [CartController::class, 'cart'])->name('cart');
+
+Route::get('/add-to-cart/{id}', [CartController::class, 'addToCart'])->name('add.to.cart');
